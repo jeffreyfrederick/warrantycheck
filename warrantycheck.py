5b279db46e6c3a05ae6b29357173738b09c8b058
Jeffrey Frederick <doubleint@gmail.com>
2022-10-09 20:51:13 -0500

    Update warrantycheck.py

--- warrantycheck.py
+++ warrantycheck.py
@@ -3,7 +3,7 @@
 #######################################################################
 # Automates checking the warranty status of Dell, HP and
 # Lenovo products from an Excel file utilizing asyncio, the 
-# Playwright API and Chromium
+# Playwright API and Chromium.
 #######################################################################
 # Author: Jeffrey Frederick
 # Date: 10/08/22
